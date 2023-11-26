de4c96c60ada59b20038ad8134762239d60f0121
Ivan <omg.dayz@gmail.com>
2023-11-26 21:12:27 +0700

    feat: catch nonexistent paths

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls")),
+    re_path(r'^.*/$', lambda request: redirect("home", permanent=True)),
 ]
