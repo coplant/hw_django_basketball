829b1ee23999d991a096d023460f52b3f99254d7
Ivan <omg.dayz@gmail.com>
2023-11-26 23:28:34 +0700

    fix: fix login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
@@ -5,10 +6,21 @@ User = get_user_model()
 
 
 class CustomUserLoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"placeholder": "Enter your password"})
-        self.fields["password"].widget.attrs.update({"placeholder": "Enter your password"})
+    username = forms.CharField(label='Логин', widget=forms.TextInput(
+        attrs={'class': 'form-input form-control', 'placeholder': 'Логин'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={'class': 'form-input form-control', 'placeholder': 'Пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def is_valid(self):
+        result = super().is_valid()
+        for x in (self.fields if '__all__' in self.errors else self.errors):
+            attrs = self.fields[x].widget.attrs
+            attrs.update({'class': attrs.get('class', '') + ' is-invalid'})
+        return result
 
 
 class CustomUserCreationForm(UserCreationForm):
