fbeb1a2634b254cf6707c97edee189ab420be798
Ivan <omg.dayz@gmail.com>
2023-11-27 15:12:10 +0700

    feat: add profile pic

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,8 @@ class CustomUser(AbstractUser):
     email = models.EmailField("Почта", unique=True, null=True, blank=True)
     phone_number = PhoneNumberField("Телефон", unique=True, null=True, blank=True)
 
+    profile_image = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("me/", views.me_view, name="me"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("register/", views.register_view, name="register"),
-]
+                  path("me/", views.me_view, name="me"),
+                  path("login/", views.login_view, name="login"),
+                  path("logout/", views.logout_view, name="logout"),
+                  path("register/", views.register_view, name="register"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
